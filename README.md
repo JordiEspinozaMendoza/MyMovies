c4ab98fea985aba5e47385325182a387bdf5e98b
Jordi <jordi8101@gmail.com>
2023-10-07 13:57:14 -0700

    feat: initial commit with initial models

---
+++ README.md
@@ -0,0 +1,72 @@
+# MyMovies Project
+
+Project for the course "Programacion Web" of Instituto Tecnologico de Tijuana.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+```
+Python 3.10.4 or higher
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Clone the repository
+
+```
+git clone https://github.com/JordiEspinozaMendoza/MyMovies
+```
+
+Create a virtual environment with venv
+
+```
+python -m venv venv
+```
+
+Activate the virtual environment
+
+For Windows
+
+```
+venv\Scripts\activate.bat
+```
+
+For Linux and MacOS
+
+```
+source venv/bin/activate
+```
+
+Install the requirements from requirements.txt
+
+```
+pip install -r requirements.txt
+```
+
+Run the server
+
+```
+python manage.py runserver
+```
+
+Run the migrations
+
+```
+python manage.py migrate
+```
+
+Open the browser and go to http://localhost:8000
+
+Enjoy!
+
+## Team members
+
+Luis Alejandro Fierro Vivian
+Jordi Espinoza Mendoza @JordiEspinozaMendoza
+
+![](https://media.tenor.com/_XgpJ0xfp-4AAAAC/writing-kermit.gif)
---
+++ app/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Studio(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField()
+    imageURL = models.URLField()
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField(max_length=4)
+    country = models.CharField(max_length=100)
+    release_date = models.DateTimeField()
+    overview = models.TextField(blank=True)
+    duration = models.IntegerField(blank=True)
+    poster_path = models.URLField()
+    studio = models.ForeignKey(Studio, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
+    tmdb_id = models.IntegerField(blank=True, unique=True)
+    credits = models.ManyToManyField(Person, through="MovieCredit")
+
+    def __str__(self):
+        return self.title
+
+
+class MovieCredit(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+
+
+class MovieReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
+    review = models.TextField(blank=True)
