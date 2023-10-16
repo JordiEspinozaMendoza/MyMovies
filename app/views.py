e1f1cbbe10a9f9e086d84f9e27158445f6f52467
Jordi <jordi8101@gmail.com>
2023-10-15 17:57:38 -0700

    feat: added static files and home screen

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def getMoviesList(request):
+    movies = Movie.objects.all()
+
+    return render(request, "movies.html", {"movies": movies})
--- myMovies/urls.py
+++ myMovies/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("movies/", views.getMoviesList)]
