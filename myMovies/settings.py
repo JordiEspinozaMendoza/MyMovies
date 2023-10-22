01c55b474eb37510f5193c9070d389956e72f19e
Jordi <jordi8101@gmail.com>
2023-10-21 20:05:41 -0700

    feat: added ALLOWED_HOSTS

--- myMovies/settings.py
+++ myMovies/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-js0ol%2b!pdmv-whjl1ue_%n&8v*m&#vn4vx#!nnk7rbi9+g51
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
